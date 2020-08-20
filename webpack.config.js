6c5c5653f37e6267409c798480adf2d760865256
Jaenal Leegenmao <jaenal.leegenmao@gmail.com>
2020-08-20 16:37:08 +0700

    init player

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ var path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/BoilerplateComponent.jsx',
+  entry: './src/mola-player.jsx',
   output: {
     path: path.resolve('lib'),
-    filename: 'BoilerplateComponent.js',
+    filename: 'mola-player.js',
     libraryTarget: 'commonjs2'
   },
   module: {
@@ -21,12 +21,25 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
-      'utils': path.resolve(__dirname, './utils')  // <-- When you build or restart dev-server, you'll get an error if the path to your utils.js file is incorrect.
+      'goog': path.resolve(__dirname, './node_modules/shaka-player/third_party/closure/goog/base.js'), // <-- When you build or restart dev-server, you'll get an error if the path to your utils.js file is incorrect.
+      'deps': path.resolve(__dirname, './node_modules/shaka-player/dist/deps.js'),
+      'asserts': path.resolve(__dirname, './node_modules/shaka-player/lib/debug/asserts.js'),
+      'muxjs': path.resolve(__dirname, './node_modules/mux.js/dist/mux.min.js'),
+      'eme': path.resolve(__dirname, './node_modules/eme-encryption-scheme-polyfill/index.js'),
+      'shaka-debug': path.resolve(__dirname, './node_modules/shaka-player/dist/shaka-player.compiled.debug.js'),
+      'shaka-prod': path.resolve(__dirname, './node_modules/shaka-player/dist/shaka-player.compiled.js'),
+      'axios': path.resolve(__dirname, './node_modules/axios/dist/axios.min.js'),
     }
   },
   plugins: [
     new webpack.ProvidePlugin({
-      'utils': 'utils'
+      goog: 'goog',
+      deps: 'deps',
+      asserts: 'asserts',
+      muxjs: 'muxjs',
+      eme: 'eme',
+      shaka: 'shaka-prod', /** ganti shaka-debug untuk development */
+      axios: 'axios',
     })
   ]
 }
